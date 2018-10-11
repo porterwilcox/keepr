5f8a4a41f902f6e27eb73bc0f6b7e98a461b0489
Porter Wilcox <wilcox.porter@gmail.com>
2018-10-10 22:39:37 -0600

    better img url

--- wwwsrc/src/components/ViewVault.vue
+++ wwwsrc/src/components/ViewVault.vue
@@ -13,7 +13,7 @@
         <v-container fill-height fluid :class="!(i % 3) ? 'light-blue darken-1' : !(i % 2) ? 'cyan darken-1' : 'teal darken-1'">
             <v-layout justify-center align-center>
                 <v-card width="400px" height="450px" class="transparent">
-                    <v-img :src="'https://pbs.twimg.com/profile_images/956698671751446528/J3ZUMcpq_400x400.jpg'"></v-img>
+                    <v-img src="https://github.com/porterwilcox/keepr/blob/master/wwwsrc/src/assets/empty.jpg?raw=true"></v-img>
                     <v-layout justify-center>
                         <v-btn @click="openVault = !openVault" round flat>Start browsing now </v-btn>
                     </v-layout>
